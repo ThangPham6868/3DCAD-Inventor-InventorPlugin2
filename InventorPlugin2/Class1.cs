7ae5b4ecaccb8f95d69831a431a3fe540698e20b
PHAM VAN THANG <IG24-258@itage.local>
2025-02-20 10:13:26 +0900

    TestGit

---
+++ InventorPlugin2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Inventor;
+using System.Windows;
+namespace InventorPlugin2
+{
+    public class Class1
+    {
+        public Inventor.Application _inventorApplication;
+        public void Activate(ApplicationAddInSite AddInSiteObject, bool FirstTime)
+        {
+            _inventorApplication = AddInSiteObject.Application;
+            //MessageBox.Show("Hello World", "Hello", System.Windows.Forms.MessageBoxButtons.OK);
+            //CreateHelloButton();
+            throw new NotImplementedException();
+        }
+
+        public void Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteCommand(int CommandID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic Automation => throw new NotImplementedException();
+    }
+}
